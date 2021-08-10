f547328ff6294c6b730b2a9d5f7a59faba80a72f
Kanat Yes <yes.kanat@gmail.com>
2021-08-10 07:37:53 +0300

    textField coded

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Button, Container, Grid, TextField } from "@material-ui/core";
 import { Context } from "../index";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestone";
+import Loader from "./Loader";
+import firebase from "firebase";
 
 export default function Chat() {
     const { auth, firestore } = useContext(Context);
     const [user] = useAuthState(auth);
+    const [value, setValue] = useState("");
+    const [messages, loading] = useCollectionData(firestore.useCollectionData("messages").orderBy("createAt"));
 
-    return <Container>Chat</Container>;
+    const sendMessage = async () => {
+        firestore.useCollectionData("messages").add({
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            text: value,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        });
+        setValue("");
+    };
+
+    if (loading) {
+        return <Loader />;
+    }
+
+    return (
+        <Container>
+            <Grid container justifyContent={"center"} style={{ height: window.innerHeight - 50, marginTop: 20 }}>
+                <div style={{ width: "80%", height: "70vh", border: "1px solid gray", overflowY: "auto" }}></div>
+                <Grid container direction={"column"} alignItems={"flex-end"} style={{ width: "80%" }}>
+                    <TextField
+                        fullWidth
+                        rowsMax={2}
+                        variant={"outlined"}
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                    />
+                    <Button onClick={sendMessage} variant={"outlined"}>
+                        Отправить
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -6,8 +6,8 @@ export default function Loader() {
         <Container>
             <Grid container style={{ height: window.innerHeight - 50 }} alignItems={"center"} justifyContent={"center"}>
                 <Grid container alignItems={"center"} direction={"column"}>
-                    <div class="loadingio-spinner-spin-r5smt7ipvv">
-                        <div class="ldio-6cgfgp0as5a">
+                    <div className="loadingio-spinner-spin-r5smt7ipvv">
+                        <div className="ldio-6cgfgp0as5a">
                             <div>
                                 <div></div>
                             </div>
